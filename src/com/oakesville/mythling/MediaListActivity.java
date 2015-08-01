8217f6cb32db326b5acce82bcf4396e2e812f738
Donald Oakes <donald@oakesville.com>
2015-08-01 15:18:27 -0600

    Fix post-deletion selection handling.

--- src/com/oakesville/mythling/MediaListActivity.java
+++ src/com/oakesville/mythling/MediaListActivity.java
@@ -153,6 +153,14 @@ public class MediaListActivity extends MediaActivity {
         }
     }
 
+    protected void handleEmptyMediaList() {
+        // go to main
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+
     public void refresh() {
         super.refresh();
         getAppSettings().setLastLoad(0);
